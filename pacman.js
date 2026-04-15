89cdd729c089d619f5e2430966a3dc5b1767df09
eugenie-0509 <ejan@immacjp2.fr>
2026-04-15 17:26:30 +0200

    Add files via upload

--- pacman.js
+++ pacman.js
@@ -55,4 +55,76 @@ pacman = {
 };
 
 //fonction algo BFS a appeler dans la fonction de rafraichissement
-function DirectionFantome() {}
+
+
+//def bfs(grille, start, end):
+    // queue = deque() #comme une liste mais optimisée 
+    // queue.append(start)
+    
+    // visited = set() #pratique pour verifier rapidement si un element y est
+    // visited.add(start)
+    
+    // parent = {}  # pour reconstruire le chemin
+    
+    // while queue: #tant que la file n est pas vide
+    //     x, y = queue.popleft() #donc si queue=deque([(2,3),(4,5)]) ba x=2 et y=3
+        
+    //     if (x, y) == end: # c est le chemin qui arrive en premier a la fin donc le plus rapide
+    //         # reconstruire le chemin
+    //         path = []
+    //         while (x, y) != start:
+    //             path.append((x, y))
+    //             x, y = parent[(x, y)]
+    //         path.reverse()
+    //         return path
+        
+    //     for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+    //         nx, ny = x + dx, y + dy
+            
+    //         # vérifier limites + mur + visite
+    //         if (0 <= nx < len(grille) and
+    //             0 <= ny < len(grille[0]) and
+    //             grille[nx][ny] == 0 and
+    //             (nx, ny) not in visited):
+                
+    //             queue.append((nx, ny))
+    //             visited.add((nx, ny))
+    //             parent[(nx, ny)] = (x, y)
+    
+    // return None  # pas de chemin
+function DirectionFantome(carte, fantomeX, fantomeY, pacmanX, pacmanY) {
+  let start = [fantomeX, fantomeY] ;
+  let end = [pacmanX, pacmanY] ;
+  let file = [] ;
+  file.push(start) ;
+
+  let visited = newSet() ;
+  visited.add(start) ;
+
+  let parent = {} ;
+
+  while (file.length > 0) {
+    let [x, y] = file.shift() ;
+
+    if ([x, y] == end) {
+      let chemin = [] ;
+      while ([x, y] != start) {
+        chemin.push([x, y]) ;
+        let x, y = parent[[x, y]] ;
+      }
+      chemin.reverse() ;
+      return chemin ;
+    }
+    for (dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]) {
+      let nx, ny = x+dx, y+dy ;
+
+      //on verifie les murs et les visités
+      if (nx >= 0 && nx < carte.length && ny >= 0 && ny < carte.length && carte[nx][ny] === 0 && !visited.has([nx, ny].toString())) {
+        file.push([nx, ny]) ;
+        visited.add([nx, ny]) ;
+        parennt[[nx, ny]] = [x, y] ;
+      }
+    }
+  }
+  return null ;
+}
