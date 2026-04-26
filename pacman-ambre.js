a1987a2b420e24cb2adf4c4c756f1301aac14a93
ambre35nsi <ambre@foucher.net>
2026-04-26 22:23:22 +0200

    démarrer jeu

--- pacman-ambre.js
+++ pacman-ambre.js
@@ -1,109 +1,23 @@
 const tailleCase = 35;
-const carteSecours = [
-	"XXXXXXXXXXXXXXXXX",
-	"XFOOOOXOOOXOOOOOX",
-	"XOXXXOXOXOXOXXXOX",
-	"XOXOOOXOXOXOOOXOX",
-	"XOXOXOXOXOXOXOXOX",
-	"XOXOXOXOXOXOXOXOX",
-	"XOXOXOOOOOOOXOXOX",
-	"XOOOOOXXXXXOOOOOX",
-	"XXXXXOOOPOOOXXXXX",
-	"XOOOOOXXXXXOOOOOX",
-	"XOXOXOOOOOOOXOXOX",
-	"XOXOXOXOXOXOXOXOX",
-	"XOXOXOXOXOXOXOXOX",
-	"XOXOOOXOXOXOOOXOX",
-	"XOXXXOXOXOXOXXXOX",
-	"XFOOOOXOOOXOOOOOX",
-	"XXXXXXXXXXXXXXXXX",
-];
 
-if (typeof pacmanX === "undefined") {
-	pacmanX = 0;
-}
-if (typeof pacmanY === "undefined") {
-	pacmanY = 0;
-}
-if (typeof fantomeX === "undefined") {
-	fantomeX = 8;
-}
-if (typeof fantomeY === "undefined") {
-	fantomeY = 8;
-}
-
-function getCarteJeu() {
-	if (typeof carte !== "undefined" && Array.isArray(carte)) {
-		return carte;
-	}
-	return carteSecours;
-}
-
-function getImagePacman() {
-	if (typeof pacmanRight !== "undefined") {
-		return pacmanRight;
-	}
-	const image = new Image();
-	image.src = "pacmanRight.png";
-	return image;
-}
-
-function getImageFantome() {
-	if (typeof redGhost !== "undefined") {
-		return redGhost;
-	}
-	const image = new Image();
-	image.src = "redGhost.png";
-	return image;
-}
-
-function trouverPositionsDepart(carteJeu) {
-	for (let y = 0; y < carteJeu.length; y++) {
-		for (let x = 0; x < carteJeu[y].length; x++) {
-			if (carteJeu[y][x] === "P") {
-				pacmanX = x;
-				pacmanY = y;
-			}
-
-			if (carteJeu[y][x] === "F") {
-				fantomeX = x;
-				fantomeY = y;
-			}
-		}
-	}
-}
-
-function chargerCarte() {
+function dessiner() {
 	const canvas = document.getElementById("game");
-	if (!canvas) {
-		return;
-	}
-
 	const ctx = canvas.getContext("2d");
-	if (!ctx) {
-		return;
-	}
 
-	const carteJeu = getCarteJeu();
-	const imagePacman = getImagePacman();
-	const imageFantome = getImageFantome();
+	// on dessine la carte case par case
+	for (let y = 0; y < carte.length; y++) {
+		for (let x = 0; x < carte[y].length; x++) {
+			let px = x * tailleCase;
+			let py = y * tailleCase;
 
-	trouverPositionsDepart(carteJeu);
-
-	for (let y = 0; y < carteJeu.length; y++) {
-		for (let x = 0; x < carteJeu[y].length; x++) {
-			const c = carteJeu[y][x];
-			const px = x * tailleCase;
-			const py = y * tailleCase;
-
-			if (c === "X") {
+			if (carte[y][x] === "X") {
 				ctx.fillStyle = "#1a2cff";
 				ctx.fillRect(px, py, tailleCase, tailleCase);
 			} else {
 				ctx.fillStyle = "#000000";
 				ctx.fillRect(px, py, tailleCase, tailleCase);
 
-				if (c === "O") {
+				if (carte[y][x] === "O") {
 					ctx.fillStyle = "#f7e85c";
 					ctx.beginPath();
 					ctx.arc(px + tailleCase / 2, py + tailleCase / 2, 4, 0, Math.PI * 2);
@@ -113,23 +27,71 @@ function chargerCarte() {
 		}
 	}
 
-	if (imagePacman.complete) {
-		ctx.drawImage(imagePacman, pacmanX * tailleCase, pacmanY * tailleCase, tailleCase, tailleCase);
-	} else {
-		imagePacman.onload = function () {
-			ctx.drawImage(imagePacman, pacmanX * tailleCase, pacmanY * tailleCase, tailleCase, tailleCase);
-		};
-	}
-
-	if (imageFantome.complete) {
-		ctx.drawImage(imageFantome, fantomeX * tailleCase, fantomeY * tailleCase, tailleCase, tailleCase);
-	} else {
-		imageFantome.onload = function () {
-			ctx.drawImage(imageFantome, fantomeX * tailleCase, fantomeY * tailleCase, tailleCase, tailleCase);
-		};
-	}
+	// on place pacman et le fantome
+	ctx.drawImage(pacmanRight, pacmanX * tailleCase, pacmanY * tailleCase, tailleCase, tailleCase);
+	ctx.drawImage(redGhost, fantomeX * tailleCase, fantomeY * tailleCase, tailleCase, tailleCase);
 }
 
-window.onload = function () {
+function chargerCarte() {
+	// on cherche les positions de depart dans la carte
+	for (let y = 0; y < carte.length; y++) {
+		for (let x = 0; x < carte[y].length; x++) {
+			if (carte[y][x] === "P") {
+				pacmanX = x;
+				pacmanY = y;
+			}
+			if (carte[y][x] === "F") {
+				fantomeX = x;
+				fantomeY = y;
+			}
+		}
+	}
+
+	dessiner();
+}
+
+function refresh() {
+	// on calcule la prochaine position de pacman
+	let nx = pacmanX;
+	let ny = pacmanY;
+
+	if (pacman.direction === "droite") nx = pacmanX + 1;
+	if (pacman.direction === "gauche") nx = pacmanX - 1;
+	if (pacman.direction === "haut")   ny = pacmanY - 1;
+	if (pacman.direction === "bas")    ny = pacmanY + 1;
+
+	// on bouge pacman seulement si c'est pas un mur
+	if (carte[ny][nx] !== "X") {
+		pacmanX = nx;
+		pacmanY = ny;
+	}
+
+	// on bouge le fantome avec le BFS
+	let chemin = DirectionFantome(carte, fantomeX, fantomeY, pacmanX, pacmanY);
+	if (chemin !== null && chemin.length > 0) {
+		fantomeX = chemin[0][1];
+		fantomeY = chemin[0][0];
+	}
+
+	dessiner();
+}
+
+// gestion des touches du clavier pour diriger pacman
+document.addEventListener("keydown", function(e) {
+	if (e.key === "ArrowRight") {
+		pacman.direction = "droite";
+	}
+	if (e.key === "ArrowLeft") {
+		pacman.direction = "gauche";
+	}
+	if (e.key === "ArrowUp") {
+		pacman.direction = "haut";
+	}
+	if (e.key === "ArrowDown") {
+		pacman.direction = "bas";
+	}
+});
+
+window.onload = function() {
 	chargerCarte();
 };
